2bd599c53276e6c34b5786a27f0ff817f1da374f
charles7668 <charles76680119@gmail.com>
2026-01-05 13:41:51 +0800

    加入專案檔案。

---
+++ GGPK-Loader/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace GGPK_Loader.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ GGPK-Loader/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GGPK_Loader.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
